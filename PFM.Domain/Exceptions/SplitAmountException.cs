b2c1f6a54cdfe42525c884e08e540fda5c6254c8
vesc0 <dveselinovic84@gmail.com>
2026-02-12 19:09:23 +0100

    minor fix

--- PFM.Domain/Exceptions/SplitAmountException.cs
+++ PFM.Domain/Exceptions/SplitAmountException.cs
@@ -3,8 +3,8 @@ namespace PFM.Domain.Exceptions
     public class SplitAmountException : BusinessRuleException
     {
         public SplitAmountException()
-            : base("split-amount-over-transaction-amount",
-            "Split amount is over transaction amount.",
+            : base("split-amount-missmatch",
+            "Split amount doesn't match transaction amount.",
             "Total amount of all splits must be equal to the transaction amount.")
         {
         }
